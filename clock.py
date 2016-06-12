4dba0887ad8d9bcee6bd20799ab1fa1782eaf350
jitto <justine.vt@gmail.com>
2016-06-12 17:54:43 -0500

    Increase time font

--- clock.py
+++ clock.py
@@ -16,14 +16,14 @@ weatherThread = threading.Thread(target=updateWeather, args=())
 weatherThread.setDaemon(True)
 weatherThread.start()
 
-def drawText(text, font, color, center, screen):
+def drawText(text, font, color, screen, **rectAttributes):
   surface = font.render(text, True, color)
-  rect = surface.get_rect(center=center)
+  rect = surface.get_rect(**rectAttributes)
   screen.blit(surface, rect)
 
 class TitleScene(scene.SceneBase):
     pygame.font.init()
-    font_big = pygame.font.Font('digital-7 (mono).ttf', 220)
+    font_big = pygame.font.Font('digital-7 (mono).ttf', 240)
     font_med = pygame.font.Font(None, 110)
     font_small = pygame.font.Font(None, 40)
     brightStart = time.time()
@@ -44,14 +44,14 @@ class TitleScene(scene.SceneBase):
     
     def Render(self, screen):
         screen.fill((0, 0, 0))
-        time_text  = time.strftime('%I:%M', time.localtime())
-        drawText(time_text, self.font_big, (255,255,255), (240,68), screen)
+        time_text  = time.strftime('%-I:%M', time.localtime())
+        drawText(time_text, self.font_big, (255,255,255), screen, midright=(478,68))
         if (type(weather_com_result) is dict and 'current_conditions' in weather_com_result):
-          drawText(weather_com_result['current_conditions']['text'], self.font_med, (255,255,255), (240,208), screen)
-          drawText(weather_com_result['current_conditions']['temperature'], self.font_med, (255,255,255), (40, 289), screen)
+          drawText(weather_com_result['current_conditions']['text'], self.font_med, (255,255,255), screen, center=(240,208))
+          drawText(weather_com_result['current_conditions']['temperature'], self.font_med, (255,255,255), screen, center=(40, 289))
           x = 129
           for forecast in weather_com_result['forecasts']:
-            drawText(forecast['high'], self.font_small, (255,255,255), (x, 279), screen)
-            drawText(forecast['low'], self.font_small, (255,255,255), (x, 302), screen)
+            drawText(forecast['high'], self.font_small, (255,255,255), screen, center=(x, 279))
+            drawText(forecast['low'], self.font_small, (255,255,255), screen, center=(x, 302))
             x = x + 40
 
