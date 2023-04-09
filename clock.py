6190c13e899db9dd889f2127b7aa20bf0bfbbe9e
jitto <jitto@users.noreply.github.com>
2023-04-09 09:20:46 -0500

    Update to wttr.in

--- clock.py
+++ clock.py
@@ -1,4 +1,5 @@
-import scene, pygame, time, pywapi, threading, sys
+import scene, pygame, time, threading, sys, requests
+
 import wiringpi
 
 weather_com_result = None
@@ -10,8 +11,12 @@ def updateWeather():
   global weather_com_result
   while True:
     try:
-      weather_com_result = pywapi.get_weather_from_weather_com('75056', 'imperial')
+#      weather_com_result = pywapi.get_weather_from_weather_com('75056', 'imperial')
+#      response = requests.get('https://api.weather.gov/gridpoints/FWD/85,116/forecast')
+      response = requests.get('https://wttr.in/?format=j1', verify=False)
+      weather_com_result = response.json()
     except:
+      weather_com_result = None
       print("Unexpected error:", sys.exc_info()[0])
     time.sleep(600)
 
@@ -49,15 +54,15 @@ class TitleScene(scene.SceneBase):
             pygame.mouse.set_visible(False)
    
     def Temperature(self, temperature):
-	if (not temperature):
-		return (255, 255, 255)
-	temp = int(temperature)
-	tempColor = (0, 255, 0)
-	if (temp < 50):
-		tempColor = (0, 255, 255)
-	elif (temp > 80):
-		tempColor = (255, 0, 0)
-	return tempColor
+        if (not temperature):
+            return (255, 255, 255)
+        temp = int(temperature)
+        tempColor = (0, 255, 0)
+        if (temp < 50):
+            tempColor = (0, 255, 255)
+        elif (temp > 80):
+            tempColor = (255, 0, 0)
+        return tempColor
 
     def Render(self, screen):
         screen.fill((0, 0, 0))
@@ -65,15 +70,17 @@ class TitleScene(scene.SceneBase):
         time_text  = time.strftime('%-I:%M', time.localtime())
         drawText(time_text, self.font_big, color, screen, midright=(478,68))
         weather = weather_com_result
-        if (type(weather) is dict and 'current_conditions' in weather):
-          drawText(weather['current_conditions']['text'], self.font_med, color, screen, center=(240,208))
-	  temp = weather['current_conditions']['temperature'] 
+        if (type(weather) is dict and 'current_condition' in weather):
+          drawText(weather['current_condition'][0]['weatherDesc'][0]['value'], self.font_med, color, screen, center=(240,208))
+          temp = str(int(weather['current_condition'][0]['temp_F']))
           drawText(temp, self.font_med, self.Temperature(temp), screen, center=(40, 289))
           x = 129
-          for forecast in weather['forecasts']:
-		high = forecast['high']
-		drawText(high, self.font_small, self.Temperature(high), screen, center=(x, 279))
-		low = forecast['low']
-		drawText(low, self.font_small, self.Temperature(low), screen, center=(x, 302))
-		x = x + 50
-
+#          for forecast in weather['periods'][0:7]:
+#            high = str(int(forecast['temperatureHigh']))
+#            drawText(high, self.font_small, self.Temperature(high), screen, center=(x, 279))
+#            low = str(int(forecast['temperatureLow']))
+#            drawText(low, self.font_small, self.Temperature(low), screen, center=(x, 302))
+#            x = x + 50
+#          drawText("as of", self.font_small, (100, 100, 100), screen, center=(420, 279))
+#          temp = weather['current_conditions']['last_updated'].split()[1]
+#          drawText(temp, self.font_small, (100, 100, 100), screen, center=(420, 302))
